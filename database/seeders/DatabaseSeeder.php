de337796e77a821bad9d80db5a52ecf9e3b2f8af
Sviatoslav <sv_dubov@ukr.net>
2021-06-13 16:07:32 +0300

    Made test Products seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $products = Product::factory(20)->create();
         // \App\Models\User::factory(10)->create();
     }
 }
