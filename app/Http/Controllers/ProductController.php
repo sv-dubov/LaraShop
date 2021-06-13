158fbbd6b38a89aaab1a09fb28fc0c423bee7bcb
Sviatoslav <sv_dubov@ukr.net>
2021-06-13 15:47:04 +0300

    Create table, model, controller, routes for Products

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return 'List of products';
+    }
+
+    public function create()
+    {
+        return 'Form for create product';
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function show($product)
+    {
+        return "Showing the product {$product}";
+    }
+
+    public function edit($product)
+    {
+        return "Form for editing the product {$product}";
+    }
+
+    public function update($product)
+    {
+        //
+    }
+
+    public function destroy($product)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('products', 'ProductController@index')->name('products.index');
+Route::get('products/create', 'ProductController@create')->name('products.create');
+Route::post('products', 'ProductController@store')->name('products.store');
+Route::get('products/{product}', 'ProductController@show')->name('products.show');
+Route::get('products/{product}/edit', 'ProductController@edit')->name('products.edit');
+Route::match(['patch', 'put'], 'products/{product}/edit', 'ProductController@update')->name('products.update');
+Route::delete('products/{product}', 'ProductController@destroy')->name('products.destroy');
