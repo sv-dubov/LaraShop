3467609f50cdb4de122ee0a12d39ec9e5f945f7e
Sviatoslav <sv_dubov@ukr.net>
2021-06-16 20:08:11 +0300

    Made editing user's profile with image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,13 +58,17 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('profile.edit') }}">
+                                        {{ __('Profile') }}
+                                    </a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'MainController@index')->name('main');
 
+Route::get('profile', 'ProfileController@edit')->name('profile.edit');
+Route::put('profile', 'ProfileController@update')->name('profile.update');
+
 Route::resource('carts', 'CartController')->only(['index']);
-Route::resource('orders', 'OrderController')->only(['create', 'store']);
+Route::resource('orders', 'OrderController')->only(['create', 'store'])->middleware(['verified']);
 Route::resource('products.carts', 'ProductCartController')->only(['store', 'destroy']);
-Route::resource('orders.payments', 'OrderPaymentController')->only(['create', 'store']);
+Route::resource('orders.payments', 'OrderPaymentController')->only(['create', 'store'])->middleware(['verified']);
 Auth::routes(['verify' => true, /*'reset' => false*/]);
 
 // Route::get('/home', 'HomeController@index')->name('home');
