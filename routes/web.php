b210ebd28671e7e7551e1fcd19be7108dc605e37
Sviatoslav <sv_dubov@ukr.net>
2021-06-16 16:32:42 +0300

    Made verifying user's email after registering

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ Route::resource('carts', 'CartController')->only(['index']);
 Route::resource('orders', 'OrderController')->only(['create', 'store']);
 Route::resource('products.carts', 'ProductCartController')->only(['store', 'destroy']);
 Route::resource('orders.payments', 'OrderPaymentController')->only(['create', 'store']);
-Auth::routes();
+Auth::routes(['verify' => true, /*'reset' => false*/]);
 
 // Route::get('/home', 'HomeController@index')->name('home');
