2d5ddd0b1128ddadcd8c09227eb797a72719b60f
Sviatoslav <sv_dubov@ukr.net>
2021-06-16 14:43:16 +0300

    Made global scope for products

---
+++ app/Models/PanelProduct.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Product;
+
+class PanelProduct extends Product
+{
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        //
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\AvailableScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,8 +11,17 @@ class Product extends Model
     use HasFactory;
 
     protected $table = 'products';
-
     protected $fillable = ['title', 'description', 'price', 'stock', 'status'];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::addGlobalScope(new AvailableScope);
+    }
 
     public function carts()
     {
